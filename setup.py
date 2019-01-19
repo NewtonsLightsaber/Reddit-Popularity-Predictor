21c567381f3575dea71a2f5021819eb074305ff8
Le Nhat Hung <Le.nhthung@gmail.com>
2019-01-18 19:37:59 -0500

    Try out cookiecutter project structure. Might remove unused packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='COMP 551 Machine Learning Project 1 - McGill University',
+    author='Le Nhat Hung',
+    license='MIT',
+)
