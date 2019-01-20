356e2199ab7bb8bee4d12805cf8435835def8c8e
Le Nhat Hung <Le.nhthung@gmail.com>
2019-01-20 14:36:59 -0500

    Split dataset into training, validaton and test sets

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,48 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import json
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(input_path, output_path):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    files = [
+        'training_data.json',
+        'validation_data.json',
+        'test_data.json',
+    ]
+
+    with open(input_path / 'proj1_data.json') as f:
+        dataset = json.load(f)
+
+    training = dataset[0:10000]
+    validation = dataset[10000:11000]
+    test = dataset[11000:12000]
+
+    sets = [
+        training,
+        validation,
+        test,
+    ]
+
+    for file, set in zip(files, sets):
+        filepath = output_path / file
+        if not filepath.is_file():
+            with open(filepath, 'w') as fout:
+                json.dump(set, fout)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
+    input_path = project_dir / 'data' / 'raw'
+    output_path = project_dir / 'data' / 'processed'
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(input_path, output_path)
