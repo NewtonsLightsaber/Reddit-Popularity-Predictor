e95e2875c1e5bedbdf8387cd7421126e9eb5cba0
Le Nhat Hung <Le.nhthung@gmail.com>
2019-01-20 15:36:08 -0500

    Add preprocessing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,25 +1,43 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 import json
 from pathlib import Path
 
 
-def main(input_path, output_path):
+project_dir = Path(__file__).resolve().parents[2]
+
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    dataset = get_dataset()
+    preprocess(dataset)
+    split_data(dataset)
+
+
+def get_dataset():
+    with open(project_dir / 'data' / 'raw' / 'proj1_data.json') as f:
+        dataset = json.load(f)
+    return dataset
+
+
+def preprocess(dataset):
+    for data in dataset:
+        is_root = data['is_root']
+        data['is_root'] = 1 if is_root else 0
+
+
+def split_data(dataset):
+    output_path = project_dir / 'data' / 'processed'
+
     files = [
         'training_data.json',
         'validation_data.json',
         'test_data.json',
     ]
-
-    with open(input_path / 'proj1_data.json') as f:
-        dataset = json.load(f)
 
     training = dataset[0:10000]
     validation = dataset[10000:11000]
@@ -32,17 +50,12 @@ def main(input_path, output_path):
     ]
 
     for file, set in zip(files, sets):
-        filepath = output_path / file
-        if not filepath.is_file():
-            with open(filepath, 'w') as fout:
-                json.dump(set, fout)
+        with open(output_path / file, 'w') as fout:
+            json.dump(set, fout, indent=4)
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    project_dir = Path(__file__).resolve().parents[2]
-    input_path = project_dir / 'data' / 'raw'
-    output_path = project_dir / 'data' / 'processed'
-
-    main(input_path, output_path)
+    main()
